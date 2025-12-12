f0c555c35e1045e3c4f280ee45b785a63b3208b4
Edgar Landivar <elandivar@yubox.com>
2025-12-12 08:48:45 -0500

    mejoras npm

--- server/index.js
+++ server/index.js
@@ -24,7 +24,16 @@ app.use('/tracks', require('./routes/tracks'));
 app.use('/maps', require('./routes/maps'));
 
 // Serve uploads (for later)
+// Serve uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
