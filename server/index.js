8339755a26841afa96728dbfb953b2e4ccbde893
Edgar Landivar <elandivar@yubox.com>
2025-12-12 08:50:28 -0500

    mejoras npm 2

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
